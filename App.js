6d7e206dfd1bf8ce2168645aabb47a97b9d9f1c6
fredfermin02 <82011662+fredfermin02@users.noreply.github.com>
2024-02-27 11:08:01 -0600

    Revert "Screen calendar view"

--- App.js
+++ App.js
@@ -1,40 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { useFonts } from 'expo-font';
-import Colors from './App/Utils/Colors';
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
-import TabNavigation from './App/Navigation/TabNavigation';
-
-
-
 
 export default function App() {
-  const [fontsLoaded, fontError] = useFonts({
-    'TitilliumWeb-Black': require('./assets/fonts/TitilliumWeb-Black.ttf'),
-    'TitilliumWeb-Italic': require('./assets/fonts/TitilliumWeb-Italic.ttf'),
-    'TitilliumWeb': require('./assets/fonts/TitilliumWeb-Regular.ttf'),
-  });
-
-  const MyTheme = {
-    ...DefaultTheme,
-    colors: {
-      background:Colors.PrimaryBrown
-    },
-  };
-
   return (
-    
-
-    
-      <NavigationContainer theme={MyTheme}>
-        
-          <TabNavigation />
-        
-          
-
-        
-      </NavigationContainer>
-    
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
